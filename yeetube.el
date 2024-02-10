3172527a6a8f1297587cf1c90eaaf594b1009f68
Thanos Apollo <public@thanosapollo.org>
2024-02-10 12:07:53 +0200

    Version bump 2.1.2

--- yeetube.el
+++ yeetube.el
@@ -5,7 +5,7 @@
 ;; Author: Thanos Apollo <public@thanosapollo.org>
 ;; Keywords: extensions youtube videos
 ;; URL: https://thanosapollo.org/projects/yeetube/
-;; Version: 2.1.2-dev
+;; Version: 2.1.2
 
 ;; Package-Requires: ((emacs "27.2") (compat "29.1.4.2"))
 
